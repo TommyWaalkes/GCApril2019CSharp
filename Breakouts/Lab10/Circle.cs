a8e0864edf54f43fb85daa31de6c65c4dbde7c94
Tommy Waalkes <Tommy@grandcircus.co>
2019-05-01 16:05:14 -0400

    Initial Commit added Lab10 Breakout too

---
+++ Breakouts/Lab10/Circle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Circle
+    {
+        private double radius;
+
+        public Circle(double r)
+        {
+            //this.radius = radius;
+            radius = r;
+        }
+
+        public double GetCircumference()
+        {
+            double output = Math.Floor(2 * Math.PI * radius);
+            return output; 
+        }
+
+        public double GetArea()
+        {
+            double area = Math.Floor(Math.PI * Math.Pow(radius, 2));
+            return area; 
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Radius: {radius}");
+            Console.WriteLine($"Area: { GetArea() }");
+            Console.WriteLine($"Circumference: { GetCircumference() }");
+        }
+       
+    }
+}
---
+++ Breakouts/Lab10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.BackgroundColor = ConsoleColor.Cyan;
+            Circle c = new Circle(5.5);
+            Console.WriteLine("Circumference: " + c.GetCircumference() );
+            Console.WriteLine("Area: " + c.GetArea());
+            Console.WriteLine("-------------");
+
+            Circle c2 = new Circle(120.16);
+            c2.PrintInfo();
+
+            //Circle doesn't exist how do we fix this error!
+            CircleAPP ca = new CircleAPP();
+
+            ca.Run();
+        }
+    }
+}
