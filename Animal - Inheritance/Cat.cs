8ed764b5ef2a320b614dc15561c643e20c3add72
Tommy Waalkes <Tommy@grandcircus.co>
2019-05-06 10:12:11 -0400

    Added it for real

---
+++ Animal - Inheritance/Cat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal_Inheritance
+{
+    class Cat : Animal
+    {
+        bool cleanLitterBox;
+        public Cat(bool cleanLitterBox, string name) : base(name)
+        {
+            this.cleanLitterBox = cleanLitterBox;
+        }
+
+        public override string GetFood()
+        {
+            return "Mice, Kibble, and lint";
+        }
+
+        public override void PrintFood()
+        {
+            Console.WriteLine($"{name} the cat eats {GetFood()}, but also likes hunting");
+        }
+    }
+}
