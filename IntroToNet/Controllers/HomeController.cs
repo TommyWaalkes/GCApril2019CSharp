e76a8378751b4308403ce203e28f9fbd7973bd9e
Tommy Waalkes <Tommy@grandcircus.co>
2019-05-21 14:11:39 -0400

    Added intro to .net

---
+++ IntroToNet/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IntroToNet.Models;
+
+namespace IntroToNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Goat()
+        {
+
+            return View();
+
+        }
+
+        public ActionResult Result(Person inputPerson)
+        {
+            if(inputPerson.firstName =="" || inputPerson.firstName == null)
+            {
+                Session["Error"] = "The first name field was empty, please enter a name";
+                return RedirectToAction("Contact");
+            }
+
+            ViewBag.Name = inputPerson.firstName;
+            ViewBag.Age = inputPerson.age;
+
+            return View();
+        }
+    }
+}
---
+++ IntroToNet/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntroToNet.Models
+{
+    public class Person
+    {
+        public string firstName { get; set; }
+        public int age { get; set; }
+    }
+}
