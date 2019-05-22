6bdf661c4504d180561ba6c10b5c97696e6dca10
Tommy Waalkes <Tommy@grandcircus.co>
2019-05-22 11:16:48 -0400

    Added .Net Validation

---
+++ Validation/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Validation.Models;
+
+namespace Validation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+
+        public ActionResult Success(Car c)
+        {
+            return View();
+        }
+
+        public ActionResult Makes()
+        {
+            return View();
+        }
+
+        public ActionResult Car(Car c)
+        {
+            List<string> AcceptibleMakes = new List<string>() { "toyota", "ford", "volkswagon" };
+            List<string> AcceptibleColors = new List<string>() { "red", "white", "blue"};
+
+                try
+                {
+                    bool goodMake = AcceptibleMakes.Contains(c.Make.ToLower());
+                    bool goodColor = AcceptibleColors.Contains(c.Color.ToLower());
+
+                    if (goodMake && goodColor)
+                    {
+                        ViewBag.Make = c.Make;
+                        ViewBag.Color = c.Color;
+                        ViewBag.Mileage = c.Mileage;
+                        ViewBag.IsUsed = c.IsUsed;
+                        return View();
+                    }
+                    else
+                    {
+                    if (!goodColor)
+                    {
+                        ViewBag.Error = "You did not input an acceptible color";
+                    }
+                    if (!goodMake)
+                    {
+                        ViewBag.Error += " You did not input an acceptible make";
+                    }
+                    return View();
+                    }
+                }
+                catch(NullReferenceException e)
+                {
+                    ViewBag.Error = "Car info not filled in, please go to index";
+                    ViewBag.StackTrace = e.StackTrace;
+                return View();
+                }
+            }
+        }
+     
+    }
---
+++ Validation/Models/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Validation.Models
+{
+    public class Car
+    {
+        [Required]
+        [Key]
+        [StringLength(30)]
+        public String Make { get; set; }
+
+        [StringLength(30, MinimumLength = 8)]
+        public String Color { get; set; }
+
+        [DisplayName("Miles")]
+        public int Mileage { get; set; }
+        public bool IsUsed { get; set; }
+    }
+}
