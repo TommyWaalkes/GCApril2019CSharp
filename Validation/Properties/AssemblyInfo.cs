1d33c5ed8d6886969b86ffe5adebd35a35c6903e
Tommy Waalkes <Tommy@grandcircus.co>
2019-05-22 13:29:07 -0400

    Added Validation fix

--- Validation/Properties/AssemblyInfo.cs
+++ Validation/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Validation")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d4b59688-ffb7-4ca0-8509-2ef4cbf0d796")]
+[assembly: Guid("3828061b-bbab-4b71-ab76-ece7a6cef189")]
 
 // Version information for an assembly consists of the following four values:
 //
