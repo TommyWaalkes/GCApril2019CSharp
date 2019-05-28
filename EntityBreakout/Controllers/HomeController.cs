c098e1d96682ae45246ae90025306a4844a34e21
Tommy Waalkes <Tommy@grandcircus.co>
2019-05-28 14:02:10 -0400

    Added Entity Breakout

---
+++ EntityBreakout/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using EntityBreakout.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityBreakout.Controllers
+{
+    public class HomeController : Controller
+    {
+        //reference to the database 
+        StarWarsEntities db = new StarWarsEntities();
+
+        public ActionResult Index()
+        {
+            List<Person> p = db.People.ToList();
+            Person output = p[0];
+            ViewBag.Person = output;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ EntityBreakout/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EntityBreakout.MvcApplication" Language="C#" %>
