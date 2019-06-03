79d802de7b202ebca4c00192ba2a7212286a5d18
Tommy Waalkes <Tommy@grandcircus.co>
2019-06-03 15:12:43 -0400

    Added breakout

---
+++ APIBreakout/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using APIBreakout.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APIBreakout.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string GetData()
+        {
+            string URL = "https://swapi.co/api/people/1/";
+
+            HttpWebRequest request = WebRequest.CreateHttp(URL);
+            //If we needed it, special setup would go here
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+
+            string APIText = rd.ReadToEnd();
+
+            return APIText;
+        }
+
+        public Person ConvertToPerson(string APIText)
+        {
+            JToken t = JToken.Parse(APIText);
+            Person p = new Person();
+            p.Name = t["name"].ToString();
+            p.SkinColor = t["skin_color"].ToString();
+            p.HomeWorld = t["homeworld"].ToString();
+            List<string> filmEndpoints = new List<string>();
+            List<JToken> jtokens=  t["films"].ToList();
+
+            foreach(JToken j in jtokens)
+            {
+                string filmEndpoint = j.ToString();
+                filmEndpoints.Add(filmEndpoint);
+            }
+
+            p.Films = filmEndpoints;
+
+            return p;
+        }
+
+        public ActionResult Index()
+        {
+            string APIText = GetData();
+            Person p = ConvertToPerson(APIText);
+
+            ViewBag.APIOutput = APIText;
+
+            return View(p);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ APIBreakout/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APIBreakout.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string SkinColor { get; set; }
+        public string BirthYear { get; set; }
+        public string HomeWorld { get; set; }
+        public List<string> Films { get; set; }
+    }
+}
