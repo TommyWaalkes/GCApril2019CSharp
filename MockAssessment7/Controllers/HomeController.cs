a94b1b39d3de0bb015cfcf6864cedfecde96f713
Tommy Waalkes <Tommy@grandcircus.co>
2019-06-10 12:20:33 -0400

    Added Mock Assessment 7

---
+++ MockAssessment7/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using MockAssessment7.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MockAssessment7.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Search(int Id)
+        {
+            string url = $"https://grandcircusco.github.io/demo-apis/donuts/{Id}.json";
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string ApiText = rd.ReadToEnd();
+
+            JToken result = JToken.Parse(ApiText);
+
+            Doughnut d = new Doughnut();
+
+            d.Id = int.Parse(result["id"].ToString());
+            d.Name = result["name"].ToString();
+            d.Calories = int.Parse(result["calories"].ToString());
+            if (result["photo"] != null)
+            {
+                d.PhotoURL = result["photo"].ToString();
+            }
+            List<JToken> e = result["extras"].ToList();
+            if (e != null)
+            {
+                d.Extras = new List<string>();
+                foreach (JToken j in e)
+                {
+                    d.Extras.Add(j.ToString());
+                }
+            }
+
+            return View(d);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MockAssessment7/Models/Doughnut.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MockAssessment7.Models
+{
+    public class Doughnut
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Calories { get; set; }
+        public string PhotoURL { get; set; }
+        public List<string> Extras { get; set; }
+    }
+}
